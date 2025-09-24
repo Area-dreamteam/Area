63180f3f0a37035ac451a18dc2c0d3bc0eda21c0
Léo Barbier <leo.barbier@epitech.eu>
2025-09-24 19:40:47 +0200

    feat: docker database

--- server/app/core/db.py
+++ server/app/core/db.py
@@ -2,11 +2,7 @@ from sqlmodel import create_engine, SQLModel
 
 
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(f"postgresql+psycopg2://root:root@pgdata:5432/area")
 
 
 
