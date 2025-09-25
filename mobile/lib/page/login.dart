0b1171d63beedd2dc1df531be5dec1606418c1aa
Boris <boris.cheng@epitech.eu>
2025-09-25 21:54:50 +0200

    feat (mobile) : homepage design finish only need connection with back

---
+++ mobile/lib/page/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'What\'s your email ?',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- mobile/lib/scaffolds/mainScaffold.dart
+++ mobile/lib/scaffolds/mainScaffold.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
-import '../page/homePage.dart';
+import '../page/login.dart';
 
-class MainScaffold extends StatefulWidget {
-  const MainScaffold({super.key});
-
-  @override
-  State<MainScaffold> createState() => _MainScaffoldState();
-}
-
-class _MainScaffoldState extends State<MainScaffold> {
-  final PageController _pageController = PageController(initialPage: 0);
+class MainPageApp extends StatelessWidget {
+  const MainPageApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
+    return const MaterialApp(home: MainPage());
   }
 }
 
-class _MainScaffoldState extends State<MainScaffold> {
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+  late PageController _pageViewController;
+  late TabController _tabController;
+
+  final List<Widget> pages = const <Widget>[
+    Center(
+      child: Image(image: AssetImage('assets/images/homepage_page_2.jpg')),
+    ),
+    Center(
+      child: Image(image: AssetImage('assets/images/homepage_page_2.jpg')),
+    ),
+    Center(
+      child: Image(image: AssetImage('assets/images/homepage_page_3.jpg')),
+    ),
+    Center(
+      child: Image(image: AssetImage('assets/images/homepage_page_4.jpg')),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageViewController = PageController();
+    _tabController = TabController(length: pages.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _pageViewController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      backgroundColor: const Color(0xFF212121),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+          padding: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.10,
+            vertical: screenWidth * 0.15,
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
@@ -36,25 +73,36 @@ class _MainScaffoldState extends State<MainScaffold> {
                     fontSize: 36,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 4,
+                    color: Colors.white,
                   ),
                 ),
               ),
-              child: PageView(
-
+              Expanded(
+                child: PageView(
+                  controller: _pageViewController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _tabController.index = index;
+                    });
+                  },
+                  children: pages,
+                ),
               ),
-
+              Center(
+                child: TabPageSelector(
+                  controller: _tabController,
+                  color: Colors.black,
+                  selectedColor: Colors.white,
+                ),
+              ),
               const SizedBox(height: 70),
-
               ElevatedButton(
                 onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
+                  _showConnectionOptions(context);
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
                   padding: const EdgeInsets.symmetric(vertical: 18),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
@@ -72,4 +120,63 @@ class _MainScaffoldState extends State<MainScaffold> {
       ),
     );
   }
+}
+
+void _showConnectionOptions(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: const Color(0xFF212121),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _buildOptionButton(
+              context,
+              'Continue with Google',
+              'assets/icons/logo_google.png',
+            ),
+            const SizedBox(height: 10),
+            _buildOptionButton(
+              context,
+              'Continue with Facebook',
+              'assets/icons/logo_facebook.png',
+            ),
+            const SizedBox(height: 10),
+            _buildOptionButton(
+              context,
+              'Continue with Email',
+              'assets/icons/logo_email.png',
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+Widget _buildOptionButton(BuildContext context, String text, String iconPath) {
+  return ElevatedButton.icon(
+    onPressed: () {
+      Navigator.of(context).pop();
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Login()),
+      );
+    },
+    icon: Image.asset(iconPath, height: 24.0, width: 24.0),
+    label: Text(
+      text,
+      style: const TextStyle(color: Colors.black, fontSize: 16.0),
+    ),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      minimumSize: const Size(double.infinity, 50),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+    ),
+  );
 }
