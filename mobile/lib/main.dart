3855849e48154f24e6f798867dd9f1bc2f4a4abf
Boris <boris.cheng@epitech.eu>
2025-09-25 13:24:48 +0200

    feat (mobile): init mobile flutter project

---
+++ mobile/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'scaffolds/mainScaffold.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AREA HomePage',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const MainScaffold(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
