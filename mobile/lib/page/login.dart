9c2852e3537c020c7e97c4ca1570417167fa7496
Boris <boris.cheng@epitech.eu>
2025-09-26 16:13:27 +0200

    fix (mobile) : color design

--- mobile/lib/page/login.dart
+++ mobile/lib/page/login.dart
@@ -1,21 +1,101 @@
 import 'package:flutter/material.dart';
+import '../page/register.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text(
-          'What\'s your email ?',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF212121),
+        body: Container(
+          margin: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _header(context),
+              _inputFields(context),
+              _signup(context),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  _header(context) {
+    return const Column(
+      children: [
+        Text(
+          "What's your email ?",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  _inputFields(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: "E-mail",
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+            fillColor: Colors.white,
+            filled: true,
+            prefixIcon: const Icon(Icons.person),
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+            fillColor: Colors.white,
+            filled: true,
+            prefixIcon: const Icon(Icons.password),
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(height: 40),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: Colors.white,
+          ),
+          child: const Text("Login", style: TextStyle(fontSize: 20, color: Colors.black)),
+        ),
+      ],
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Dont have an account?",
+          style: TextStyle(color: Colors.white),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Register()),
+            );
+          },
+          child: const Text("Sign Up", style: TextStyle(color: Colors.white)),
+        ),
+      ],
     );
   }
 }
