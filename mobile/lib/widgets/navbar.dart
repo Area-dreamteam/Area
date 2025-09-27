e613e17102723473839a5c76427b68a5252aa7f1
Boris <boris.cheng@epitech.eu>
2025-09-27 14:19:24 +0200

    feat (mobile) : animation navbar and Card for the area / service

--- mobile/lib/widgets/navbar.dart
+++ mobile/lib/widgets/navbar.dart
@@ -1,48 +1,46 @@
 import 'package:flutter/material.dart';
 
-class NavigationBarApp extends StatelessWidget {
-  final int selectedIndex;
-  final ValueChanged<int> onItemSelected;
+class MyBottomNavigationBar extends StatefulWidget {
+  const MyBottomNavigationBar({super.key});
 
-  const NavigationBarApp({
-    super.key,
-    required this.selectedIndex,
-    required this.onItemSelected,
-  });
+  @override
+  State<MyBottomNavigationBar> createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0: Home', style: optionStyle, selectionColor: Colors.black,),
+    Text('Index 1: Explore', style: optionStyle),
+    Text('Index 2: Create', style: optionStyle),
+    Text('Index: 3: My Area', style: optionStyle),
+    Text('Index: 4: Profile', style: optionStyle),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      selectedIndex: selectedIndex,
-      onDestinationSelected: onItemSelected,
-      indicatorColor: Colors.blue,
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.home_outlined),
-          selectedIcon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.search_outlined),
-          selectedIcon: Icon(Icons.search),
-          label: 'Explore',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.create_outlined),
-          selectedIcon: Icon(Icons.create),
-          label: 'Create',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.menu_outlined),
-          selectedIcon: Icon(Icons.menu),
-          label: 'My AREA',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.account_circle_outlined),
-          selectedIcon: Icon(Icons.account_circle),
-          label: 'Profile',
-        ),
-      ],
+    return Scaffold(
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', backgroundColor: Colors.black),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Explore', backgroundColor: Colors.black),
+          BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: 'Create', backgroundColor: Colors.black),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'My Area', backgroundColor: Colors.black),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile', backgroundColor: Colors.black)
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
