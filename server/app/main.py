bdf153290a78a5ba9990e5255f3d6799b25f3caa
FireSky33 <gauthier.fagot@epitech.eu>
2025-09-28 18:55:21 +0200

    feat(server): add a new volume for images in the docker compose

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 from contextlib import asynccontextmanager
 
 from core.loader import load_services_catalog, load_services_config
 from core.db import init_db
 from core.logger import logger
+from api import about
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,9 +19,11 @@ async def lifespan(app: FastAPI):
     logger.info("Server shutting down...")
 
 app = FastAPI(lifespan=lifespan, title="AREA API", version="1.0.0")
+ 
+app.mount("/images", StaticFiles(directory="/images"), name='images')
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to AREA API"}
 
-# app.include_router(auth.router, tags=["auth"], prefix='/auth')
+app.include_router(about.router, tags=["About"])
