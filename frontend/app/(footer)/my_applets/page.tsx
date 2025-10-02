2ff83763f921e3f886573c8a0d59f16d55d1a679
Idriss Akbi <idriss.akbi@epitech.eu>
2025-10-02 13:50:37 +0200

    feat: added a footer to the settings pages and applet/my_applets

---
+++ frontend/app/(footer)/my_applets/page.tsx
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** Area_Mirroring
+** File description:
+** page
+*/
+
+export default function My_applet()
+{
+    return (
+        <div>
+            Empty page
+        </div>
+    )
+}
