b7603e1bcca5e341d47bdef82b2b9aed4499a8ff
Idriss Akbi <idriss.akbi@epitech.eu>
2025-10-02 14:28:36 +0200

    feat: searching bar fully functionnal for the explore page (services, applets and all)

--- frontend/app/(connected-layout)/explore/page.tsx
+++ frontend/app/(connected-layout)/explore/page.tsx
@@ -77,11 +77,6 @@ const applets = [
     }
 ]
 
-const stories = [
-    {
-    }
-]
-
 function taskbarButton(buttonName: string, selected: string,
     setPage: (str: string) => void, enable: boolean)
 {
@@ -121,50 +116,87 @@ function Filter()
     )
 }
 
-function Services()
+interface SearchProp {
+    search?: string
+}
+
+function Services({search = ""}: SearchProp)
 {
-    const serviceBlocks = services.map((service) => (
-        <div key={service.id} className="rounded-xl w-[250px] h-[300px]" style={{ backgroundColor: service.color }}>
-            <Image alt="service's logo" src={service.logo} width={4000} height={4000} className="rounded-xl w-[250px] h-[250px]"/>
-            <div className="flex justify-center">
-                <p className="font-bold text-white text-[20px] m-[20px]">{service.name}</p>
+    const filteredServices = services.filter(service =>
+        service.name.toLowerCase().includes(search.toLowerCase())
+    );
+    const nbServices = filteredServices.length;
+    const serviceBlocks = services.map((service) => ((
+        service.name.toLowerCase().includes(search.toLowerCase()) ?
+        (
+            <div key={service.id} className="rounded-xl w-[250px] h-[300px]" style={{ backgroundColor: service.color }}>
+                <Image alt="service's logo" src={service.logo} width={4000} height={4000} className="rounded-xl w-[250px] h-[250px]"/>
+                <div className="flex justify-center">
+                    <p className="font-bold text-white text-[20px] m-[20px]">{service.name}</p>
+                </div>
             </div>
-        </div>
-    ))
+        ) : (
+            ""
+        )
+    )))
 
     return (
-        <div className="mt-[50px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
-            {serviceBlocks}
+        <div>
+            {nbServices != 0 ? (
+                <div className="mt-[50px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
+                    {serviceBlocks} 
+                </div>
+            ) : (
+                <p className="flex justify-center text-[20px] mt-[20px]">
+                    No service found.
+                </p>
+            )}
         </div>
     )
 }
 
-function Applets()
+function Applets({search = ""}: SearchProp)
 {
+    const filteredApplets = services.filter(service =>
+        service.name.toLowerCase().includes(search.toLowerCase())
+    );
+    const nbApplets = filteredApplets.length;
     const appletBlocks = applets.map((applet) => (
-        <div key={applet.id} className="rounded-xl w-[250px] h-[300px]" style={{ backgroundColor: applet.color }}>
-            <div className="flex justify-center">
-                <p className="font-bold text-white text-[20px] m-[20px]">{applet.name}</p>
+        applet.name.toLowerCase().includes(search.toLowerCase()) ?
+        (
+            <div key={applet.id} className="rounded-xl w-[250px] h-[300px]" style={{ backgroundColor: applet.color }}>
+                <div className="flex justify-center">
+                    <p className="font-bold text-white text-[20px] m-[20px]">{applet.name}</p>
+                </div>
             </div>
-        </div>
-    ))
+        ) : (
+        ""
+    )))
 
     return (
-        <div className="mt-[50px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
-            {appletBlocks}
+        <div>
+            {nbApplets != 0 ? (
+                <div className="mt-[50px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
+                {appletBlocks}
+            </div>
+            ) : (
+                <p className="flex justify-center text-[20px] mt-[20px]">
+                    No applet found.
+                </p>
+            )}
         </div>
     )
 }
 
-function All()
+function All({search = ""}: SearchProp)
 {
     return (
         <div>
             <h1 className="flex justify-center font-bold text-[25px]"> Services </h1>
-            <Services/>
+            <Services search={search}/>
             <br/>
             <h1 className="flex justify-center font-bold text-[25px]"> Applets </h1>
-            <Applets/>
+            <Applets search={search}/>
         </div>
     )
 }
@@ -172,6 +204,7 @@ function All()
 export default function Explore()
 {
     const [page, setPage] = useState("All");
+    const [searched, setSearched] = useState("");
 
     return (
         <div>
@@ -181,19 +214,18 @@ export default function Explore()
                     {taskbarButton("All", page, setPage, true)}
                     {taskbarButton("Applets", page, setPage, true)}
                     {taskbarButton("Services", page, setPage, true)}
-                    {taskbarButton("Stories", page, setPage, false)}
                 </div>
             </div>
             <br/>
             <div className="flex justify-center">
-                <Input className="w-[400px]" placeholder="Search Apllets or Services"/>
+                <Input className="w-[400px]" placeholder="Search Applets or Services" onChange={(e) => setSearched(e.target.value)}/>
             </div>
             <br/>
             {page == "Services"  && <Filter/>}
             <div className="flex justify-center">
-                {page == "Services" && <Services/>}
-                {page == "Applets" && <Applets/>}
-                {page == "All" && <All/>}
+                {page == "Services" && <Services search={searched}/>}
+                {page == "Applets" && <Applets search={searched}/>}
+                {page == "All" && <All search={searched}/>}
             </div>
         </div>
     )
