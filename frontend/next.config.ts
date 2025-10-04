84077b7ce0f70b30a763f4d5864837270848f99e
SIMLUKE <luc.simon@epitech.eu>
2025-10-05 00:40:43 +0200

    feat(docker): added backend prod dockerfile
    + fixed overall docker compose for production pipeline

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/client.apk",
+        destination: "/api/download/client",
+      },
+    ];
+  },
   output: "standalone",
 };
 
+module.exports = nextConfig;
 export default nextConfig;
--- server/app/main.py
+++ server/app/main.py
@@ -8,23 +8,27 @@ from core.db import init_db
 from core.logger import logger
 from api import about, auth, services, actions, reactions, areas, users
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info("Server starting...")
-    catalog: list[dict]= load_services_catalog()
+    catalog: list[dict] = load_services_catalog()
     config: list[dict] = load_services_config()
     app.state.services_config = config
     init_db(catalog)
     yield
     logger.info("Server shutting down...")
 
+
 app = FastAPI(lifespan=lifespan, title="AREA API", version="1.0.0")
- 
-app.mount("/images", StaticFiles(directory="/images"), name='images')
+
+app.mount("/images", StaticFiles(directory="./images"), name="images")
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to AREA API"}
+
 
 app.include_router(about.router, tags=["about"])
 app.include_router(auth.router, tags=["auth"], prefix="/auth")
