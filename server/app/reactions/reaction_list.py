43b883b8221ee285ea2f2426cf3d8f898133ab27
SIMLUKE <luc.simon@epitech.eu>
2025-10-08 14:06:36 +0200

    feat(reactions): added gmail sending reaction

--- server/app/reactions/reaction_list.py
+++ server/app/reactions/reaction_list.py
@@ -3,8 +3,10 @@ from dependencies.db import SessionDep
 
 from reactions.debug_reaction import debug_reaction
 from reactions.create_task import create_task
+from reactions.gmail_reaction import gmail_reaction
 
 reaction_list: Dict[str, Callable[[SessionDep, int, list], None]] = {
     "Debug_reaction": debug_reaction,
-    "create_task": create_task
+    "create_task": create_task,
+    "send_email": gmail_reaction,
 }
