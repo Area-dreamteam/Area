e13d16ed02a12d3930721b56698abf3603f97e3c
Idriss Dupoisot <idriss.akbi@epitech.eu>
2025-10-18 14:18:39 +0200

    refactor: log in and register synthetize to one component and both responsive

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -5,97 +5,10 @@
 ** login
 */
 
-'use client'
-
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert"
-import Link from "next/link"
-import Image from "next/image"
-import { useEffect, useState } from "react"
-import { useRouter } from 'next/navigation'
-import { AlertCircleIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { fetchLogin } from "../functions/fetch"
-import { Password, Mail } from "../components/Forms"
-import redirectToPage from "../functions/redirections"
-import { fetchAvailableOAuth, OAuthInfo, redirectOauth } from "../functions/oauth"
+import Logins from "../components/Logins"
 
 export default function Login() {
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState(false);
-  const [password, setPassword] = useState("");
-  const [logins, setLogins] = useState<[OAuthInfo]>()
-
-  async function sendLoginForm(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const success: boolean = await fetchLogin(email, password);
-
-    if (success) {
-      router.push("/explore");
-    } else
-      setError(true);
-  }
-  useEffect(() => {
-    fetchAvailableOAuth(setLogins)
-  }, [])
-
-  return (
-    <div className="bg-[#FFFFFF] h-screen font-bold">
-      <div>
-        <p onClick={() => redirectToPage("/")} className="w-[200px] mx-auto flex justify-center text-[100px] text-black hover:text-[#424242] hover:cursor-pointer">Area</p>
-        <h1 className="flex justify-center text-[50px] text-black mb-10">Log in</h1>
-        <form onSubmit={(e) => sendLoginForm(e)}>
-          <div className="flex justify-center">
-            <Mail onChange={setEmail} />
-          </div>
-          <div className="flex justify-center">
-            <Password w={"300px"} onChange={setPassword} />
-          </div>
-          {error &&
-            (<Alert variant="destructive" className="bg-red-100 rounded-4xl mb-[20px] mr-[20px] w-[300px] mx-auto">
-              <AlertCircleIcon />
-              <AlertTitle>Logins incorrect.</AlertTitle>
-              <AlertDescription>
-                <p>Your email or password seems to be wrong. Please try again.</p>
-              </AlertDescription>
-            </Alert>)
-          }
-
-          <div className="flex justify-center">
-            <Link href="/passwords/forgot" className="flex justify-center mb-[30px] text-black text-center text-[20px] hover:text-[#676767] underline">
-              Forgot your password ?
-            </Link>
-          </div>
-          <div className="flex justify-center">
-            <Button className="flex justify-center mb-3 bg-[#000000] text-white hover:text-black text-[40px] hover:bg-[#73bbff] rounded-full w-[350px] h-[100px] font-bold pt-2.5 hover:cursor-pointer" type="submit">
-              Log in
-            </Button>
-          </div>
-        </form>
-        <p className="flex justify-center">Or</p>
-        <div className="flex justify-center">
-          <hr className="w-[350px] outline-[1px] mb-[30px]" style={{ color: "#4400ff" }} />
-        </div>
-        <li>
-          {logins && logins.map((l) => {
-            return (
-              <ul className="flex justify-center" key={l.name}>
-                <Button onClick={() => { redirectOauth(l.name) }} className="flex justify-center mb-[20px] bg-white text-black text-[30px] hover:bg-[#e4e4e4] rounded-full w-[450px] h-[70px] hover:border-[#000000] border-[#e4e4e4] border-[2px] outline-[1px] pt-[10px]" >
-                  Continue with {l.name}
-                </Button>
-              </ul>
-
-            )
-          })}
-        </li>
-        <p className="flex justify-center mb-3 text-black text-center text-[20px]">
-          New to Area ? <Link href="/register" className="hover:text-[#4400ff] underline">Sign up here.</Link>
-        </p>
-      </div>
-    </div>
-  )
+    return (
+        <Logins isRegister={false}/>
+    )
 }
--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -5,94 +5,10 @@
 ** register
 */
 
-'use client'
-
-import { Password, Mail } from "../components/Forms"
-import { fetchRegister } from "../functions/fetch"
-import { AlertCircleIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from "react"
-import Image from "next/image"
-import Link from "next/link"
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert"
-import redirectToPage from "../functions/redirections"
-import { fetchAvailableOAuth, OAuthInfo, redirectOauth } from "../functions/oauth"
+import Logins from "../components/Logins";
 
 export default function Register() {
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState(false);
-  const [password, setPassword] = useState("");
-  const [logins, setLogins] = useState<[OAuthInfo]>();
-
-  useEffect(() => {
-    fetchAvailableOAuth(setLogins)
-  }, [])
-
-
-  async function sendRegisterForm(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const success: boolean = await fetchRegister(email, password);
-
-    if (success)
-      router.push("/login");
-    else
-      setError(true);
-  }
-
-  return (
-    <div className="bg-white h-screen font-bold">
-      <div>
-        <p onClick={() => redirectToPage("/")} className="w-[200px] mx-auto centered title logo">Area</p>
-        <h1 className="title text-black md:mb-10 mb-5">Register</h1>
-        <form onSubmit={(e) => sendRegisterForm(e)}>
-          <div className="centered md:mb-3 mb-1">
-            <Mail onChange={setEmail} />
-          </div>
-          <div className="centered">
-            <Password w={"300px"} onChange={setPassword} />
-          </div>
-          {error &&
-            <Alert variant="destructive" className="bg-red-100 rounded-4xl mb-5 mr-[20px] w-[300px] mx-auto">
-              <AlertCircleIcon />
-              <AlertTitle>Sorry, this account already exist.</AlertTitle>
-            </Alert>
-          }
-          <div className="centered">
-            <button className="rounded-button inverted m-[5%]" type="submit">
-              Get started
-            </button>
-          </div >
-        </form>
-      </div>
-      <p className="centered mb-[2%] simple-text">Or</p>
-      <div className="centered">
-        <hr className="w-[40%] mb-[2%]" style={{ color: "#4400ff" }} />
-      </div>
-      <li>
-        {logins && logins.map((l) => {
-          return (
-            <ul className="centered" key={l.name}>
-              <button onClick={() => { redirectOauth(l.name) }} className="rounded-button border-1 m-[5%]" >
-                Continue with {l.name}
-              </button>
-            </ul>
-          )
-        })}
-      </li>
-      <div className="centered">
-        <p className="mb-[10%] text-black text-center simple-text">
-          Already have an account ?
-          <Link href="/login" className="activate-link">
-            Log in here.
-          </Link>
-        </p>
-      </div>
-    </div>
-  )
+    return (
+        <Logins isRegister={true}/>
+    )
 }
