f919fb2b5e2b512f94bc385b55d80f707f23d06f
SIMLUKE <luc.simon@epitech.eu>
2025-10-27 11:42:30 +0100

    fix(mobile): fixed build for deployement

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,24 +1,27 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const API_URL =
-  process.env.BACKEND_URL || "https://area-prod-back.onrender.com";
+const API_URL = process.env.BACKEND_URL || 'https://area-prod-back.onrender.com'
 
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/backend/:path*",
+        source: '/api/backend/:path*',
         destination: `${API_URL}/:path*`,
       },
       {
-        source: "/client.apk",
-        destination: "/api/download/client",
+        source: '/client.apk',
+        destination: '/api/download/client',
       },
-    ];
+      {
+        source: '/about.json',
+        destination: '/api/backend/about.json',
+      },
+    ]
   },
-  output: "standalone",
-  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
-};
+  output: 'standalone',
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+}
 
-module.exports = nextConfig;
-export default nextConfig;
+module.exports = nextConfig
+export default nextConfig
