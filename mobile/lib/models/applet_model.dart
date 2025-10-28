5dad1fe8e58f2505bfaca17f081f9664feb85cca
Boris <boris.cheng@epitech.eu>
2025-10-28 15:47:20 +0100

    feat(my_applet): new class my_applet

--- mobile/lib/models/applet_model.dart
+++ mobile/lib/models/applet_model.dart
@@ -1,5 +1,18 @@
-import 'applet_user.dart';
 import 'service_info_model.dart';
+
+class AppletUser {
+  final int id;
+  final String name;
+
+  AppletUser({required this.id, required this.name});
+
+  factory AppletUser.fromJson(Map<String, dynamic> json) {
+    return AppletUser(
+      id: json['id'] as int,
+      name: json['name'] as String,
+    );
+  }
+}
 
 class AppletModel {
   final int id;
