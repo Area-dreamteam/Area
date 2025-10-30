217cc1203e83426509c88143bffc54c484557238
SIMLUKE <luc.simon@epitech.eu>
2025-10-30 17:26:39 +0100

    fix(backend): fixed date and time as debug

--- server/app/services/date_and_time.py
+++ server/app/services/date_and_time.py
@@ -18,7 +18,8 @@ class DateAndTime(Service):
             ]
             super().__init__(
                 "Déclenché une fois par heure à :00, :15, :30 ou :45 minutes après l'heure.",
-                config_schema, "* * * * *"
+                config_schema,
+                "* * * * *",
             )
 
         def check(
@@ -28,12 +29,15 @@ class DateAndTime(Service):
             selected_minute: int = int(
                 get_component(area_action.config, "select_interval", "values")
             )
-            return True
-            # if selected_minute == current_time.minute:
-            #     return True
-            # return False
+            if selected_minute == current_time.minute:
+                return True
+            return False
 
     def __init__(self) -> None:
         super().__init__(
-            "Service Date&Time", "Time", "#4a4d4b", "/images/DateAndTime_logo.webp", False
+            "Service Date&Time",
+            "Time",
+            "#4a4d4b",
+            "/images/DateAndTime_logo.webp",
+            False,
         )
